6f8f4326aa29d429dbd513a225634660a761436d
KokiMatsumura <matsumura.koki1@gmail.com>
2019-07-05 02:14:07 +0900

    fix: clang-formatしたファイルを先にコピーするように修正

--- docker/home/build.py
+++ docker/home/build.py
@@ -8,19 +8,19 @@ import subprocess
 BASE_DIRS = ["/hrp2/sdk/common/library", "/hrp2/sdk/workspace"]
 
 # TODO: ミドルウェアのビルドが失敗する原因を突き止めて有効にする
-# print('**** clang-format ****')
-# for bd in BASE_DIRS:
-#     cpp_list = glob.glob(bd+'/**/*.cpp', recursive=True)
-#     header_list = glob.glob(bd+'/**/*.h', recursive=True)
-#     cmd = "clang-format -style=file -i {}"
+print('**** clang-format ****')
+for bd in BASE_DIRS:
+    cpp_list = glob.glob(bd+'/**/*.cpp', recursive=True)
+    header_list = glob.glob(bd+'/**/*.h', recursive=True)
+    cmd = "clang-format -style=file -i {}"
 
-#     for cpp in cpp_list:
-#         print(cmd.format(cpp))
-#         subprocess.call(cmd.format(cpp), shell=True)
+    for cpp in cpp_list:
+        print(cmd.format(cpp))
+        subprocess.call(cmd.format(cpp), shell=True)
 
-#     for header in header_list:
-#         print(cmd.format(header))
-#         subprocess.call(cmd.format(header), shell=True)
+    for header in header_list:
+        print(cmd.format(header))
+        subprocess.call(cmd.format(header), shell=True)
 
 
 print('**** EV3 build ****')
@@ -29,6 +29,10 @@ name = "app"
 if ENV in os.environ:
     name = os.environ[ENV]
 
+subprocess.call("rm -r /gateway/library/RyujiEv3", shell=True)
+subprocess.call("cp -r /hrp2/sdk/common/library/RyujiEv3 /gateway/library", shell=True)
+subprocess.call("rm -r /gateway/workspace/RO2020EV3", shell=True)
+subprocess.call("cp -r /hrp2/sdk/workspace/RO2020EV3 /gateway/workspace", shell=True)
 
 now = datetime.now(timezone(timedelta(hours=+9), 'JST'))
 name += now.strftime('%m%d%H%M%S')
@@ -37,5 +41,3 @@ subprocess.call("make app=RO2020EV3", shell=True)
 if not os.path.exists("/gateway/production"):
     os.mkdir("/gateway/production")
 subprocess.call("mv /hrp2/sdk/workspace/app /gateway/production/" + name, shell=True)
-subprocess.call("mv /hrp2/sdk/common/library/RyujiEv3 /gateway/library/RyujiEv3", shell=True)
-subprocess.call("mv /hrp2/sdk/workspace/RO2020EV3 /gateway/workspace/RO2020EV3", shell=True)
