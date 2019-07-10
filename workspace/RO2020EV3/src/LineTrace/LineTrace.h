d3ade84e2c6aad0d8508c8faa41e13e5dbb4d0e1
Nagisa Takeuchi <takeuchi.nagisa1@gmail.com>
2019-07-10 21:33:57 +0900

    update:PID値をconstに変更

--- workspace/RO2020EV3/src/LineTrace/LineTrace.h
+++ workspace/RO2020EV3/src/LineTrace/LineTrace.h
@@ -1,7 +1,8 @@
-//LineTrace.h
+// LineTrace.h
 
-//includeファイル
+// includeファイル
 #include <Drive.h>
+#include <array>
 #include "Enum.h"
 
 //クラス定義
@@ -9,17 +10,19 @@
 //機能名    :MWのLineTraceにPID値をセット
 //機能概要  :各状況(カーブ等)に最適化されたPID値をセットする
 
-
 class LineTrace {
  public:
-  LineTrace();                 //コンストラクタ
-  ~LineTrace();                //デストラクタ
-  int lineTraceDrive(MapState runState); //マップ状況に合わせて最適なPID値をセット
+  LineTrace();   //コンストラクタ
+  ~LineTrace();  //デストラクタ
+  int lineTraceDrive(MapState runState);  //マップ状況に合わせて最適なPID値をセット
 
  private:
-  PID m_straightPID;            //直線走行用PID値
-  PID m_smallRightTurn;         //右小回り用PID値
-  PID m_largeRightTurn;         //右大回り用PID値
-  PID m_smallLeftTurn;          //左小回り用PID値
-  PID m_largeLeftTurn;          //左大回り用PID値
+  //各種PID格納配列(順番はMapState(enum)に準拠)
+  const std::array<PID, 5> m_PID = {
+      {0.0f, 0.0f, 0.0f},  //直線走行用PID値//(仮)
+      {0.0f, 0.0f, 0.0f},  //右小回り用PID値//(仮)
+      {0.0f, 0.0f, 0.0f},  //左小回り用PID値//(仮)
+      {0.0f, 0.0f, 0.0f},  //右大回り用PID値//(仮)
+      {0.0f, 0.0f, 0.0f}   //左大回り用PID値//(仮)
+  };
 };
