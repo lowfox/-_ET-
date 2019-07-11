49159dc3f012582aaae0e8e8a12ce4a9540b0e3b
KokiMatsumura <matsumura.koki1@gmail.com>
2019-07-11 18:20:27 +0900

    update: macでもwin homeでもwin proでも使えるbuild.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-if [ $# -ne 2 ]; then
-    echo "方法: ./build.sh [ローカルev3リポジトリの絶対パス] [生成するバイナリのプレフィックス]"
-    echo "おじさんの例: ./build.sh /Users/cr0w/dev/src/github.com/ro-2020/ev3 kusoapp"
-    exit 1
+
+EV3_MY_APP_NAME="app"
+if [ $# -ne 1 ]; then
+    echo "======================================================================"
+    echo "| バイナリのプレフィックスをappから変更することもできます"
+    echo "| 方法: ./build.sh [バイナリのプレフィックス]"
+    echo "| 例: バイナリのプレフィックスをkusoappにする -> ./build.sh kusoapp"
+    echo "======================================================================"
+else
+    EV3_MY_APP_NAME=$1
 fi
 
-echo "[Ads] GO IS SUPER NICE LANGUAGE..."
-echo "[+] ローカルev3リポジトリの絶対パス=$1"
-echo "[+] 生成するバイナリのプレフィックス=$2"
+echo "[*] 生成するバイナリのプレフィックス=$EV3_MY_APP_NAME"
 
-docker build . -t ev3dev:v1
-docker run -e EV3_APP_NAME=$2 -v $1:/gateway ev3dev:v1
+if [ "$(uname)" == 'Darwin' ]; then
+  docker build . -t ev3dev:v1
+  docker run -e EV3_APP_NAME=$EV3_MY_APP_NAME -v $(pwd):/gateway ev3dev:v1
+else
+  docker build . -t ev3dev:v1
+  docker run -e EV3_APP_NAME=$EV3_MY_APP_NAME -v /$PWD:/gateway ev3dev:v1
+fi
