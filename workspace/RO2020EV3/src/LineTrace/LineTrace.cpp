1146c9ceeecc65bfb008a07b2c091f0c259905f7
Nagisa Takeuchi <takeuchi.nagisa1@gmail.com>
2019-07-12 00:40:30 +0900

    update:エラーチェックの修正

--- workspace/RO2020EV3/src/LineTrace/LineTrace.cpp
+++ workspace/RO2020EV3/src/LineTrace/LineTrace.cpp
@@ -8,8 +8,8 @@ LineTrace::LineTrace() {}
 LineTrace::~LineTrace() {}
 
 int LineTrace::lineTraceDrive(MapState runState) {
-  //エラーチェック(runStateに0～5以外の値がセットされている場合)
-  if (0 > runState > 5) {
+  //エラーチェック(runStateに1以上StateEnd未満の値がセットされていない場合)
+  if (0 > runState || StateEnd <= runState) {
     return -1;
   }
 
