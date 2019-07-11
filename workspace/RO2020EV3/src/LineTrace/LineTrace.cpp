573ec61903a44ca3c86025dbb58f3cb772b6e3b6
Nagisa Takeuchi <takeuchi.nagisa1@gmail.com>
2019-07-11 17:25:33 +0900

    update:不要なIncludeの削除及びエラーチェック追加

--- workspace/RO2020EV3/src/LineTrace/LineTrace.cpp
+++ workspace/RO2020EV3/src/LineTrace/LineTrace.cpp
@@ -2,18 +2,17 @@
 
 // includeファイル
 #include "LineTrace.h"
-#include <Drive.h>
 
-LineTrace::LineTrace()
-{
-}
+LineTrace::LineTrace() {}
 
-LineTrace::~LineTrace()
-{
-}
+LineTrace::~LineTrace() {}
 
-int LineTrace::lineTraceDrive(MapState runState)
-{
+int LineTrace::lineTraceDrive(MapState runState) {
+  //エラーチェック(runStateに0～5以外の値がセットされている場合)
+  if (0 > runState > 5) {
+    return -1;
+  }
+
   // runStateによって指定されたPID値をセット
   Drive::LineTrace::SetPID(m_PID[runState]);
   return 0;
