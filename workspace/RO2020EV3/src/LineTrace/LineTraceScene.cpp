76fc9f15b6770fb810f5480e79c187bc1cf21f44
Nagisa Takeuchi <takeuchi.nagisa1@gmail.com>
2019-07-17 20:17:01 +0900

    update:書きこまれていたテストコードを削除(原点復帰)

--- workspace/RO2020EV3/src/LineTrace/LineTraceScene.cpp
+++ workspace/RO2020EV3/src/LineTrace/LineTraceScene.cpp
@@ -1,18 +1,6 @@
 #include "LineTraceScene.h"
 #include <Config.h>
 
-//テストコード*****************************************
-#define CASE_MAX 9  //テストケースの最大数
-#include "LineTrace.h"
-#include <Logger.h>
-//テストケース格納構造体定義
-struct Test {
-  MapState state;  //試験用引数
-                    int trueResult;  //引数に対する期待値
-};
-
-//テストコード******************************************
-
 LineTraceScene::LineTraceScene(ISceneChanger* sceneChanger) :IScene(sceneChanger)
 {
 }
@@ -24,34 +12,6 @@ bool LineTraceScene::init()
 
 bool LineTraceScene::run()
 {
-	// LineTraceCode
 
-  //テストコードここから********************************************
-  //テスト用引数とその期待値一覧
-        
-std::array<Test, CASE_MAX> testCase = {{{(MapState)-1, -1},
-                                        {Straight, 0},
-                                        {SmallRightTurn, 0},
-                                        {SmallLeftTurn, 0},
-                                        {LargeRightTurn, 0},
-                                        {LargeLeftTurn, 0},
-                                        {Goal, 0},
-                                        {StateEnd, -1},
-                                        {(MapState)7, -1}}};
-
-  // LineTraceクラス生成
-  LineTrace lineTrace;
-  int result = 0;
-
-  for (int cnt = 0; cnt < CASE_MAX; cnt++) {
-    result = lineTrace.lineTraceDrive(testCase[cnt].state);
-    //実際の戻り値が期待した戻り値と不一致だった場合
-    if (result != testCase[cnt].trueResult) {
-      EV3_LOG_ERROR("Cnt = %d is error\n",cnt);
-    }
-  }
-  EV3_LOG("End\n");
-  //テストコードここまで*************************************************
-
-	return change(LINETRACE_NEXT_SCENE);
+  return change(LINETRACE_NEXT_SCENE);
 }
