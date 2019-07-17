8fe74bd1d223ab7f9386c5e1e110ce1f23a4bd4a
Nagisa Takeuchi <takeuchi.nagisa1@gmail.com>
2019-07-18 01:36:23 +0900

    update:関数ヘッダコメント追加

--- workspace/RO2020EV3/src/LineTrace/ColorDetection.h
+++ workspace/RO2020EV3/src/LineTrace/ColorDetection.h
@@ -12,6 +12,16 @@ class ColorDetection {
  public:
   ColorDetection();      //コンストラクタ
   ~ColorDetection();     //デストラクタ
+  //関数名   :getBlueColor
+  //機能名   :青色検知
+  //機能概要 :カラーセンサーで青色を検知したか否かを戻り値で返す
+  //引数     :無し
+  //戻り値   :bool       :青色検知 = true
   bool getBlueColor();   //青色検知関数
+  //関数名   :getBlackColor
+  //機能名   :黒色検知
+  //機能概要 :カラーセンサーで黒色を検知したか否かを戻り値で返す
+  //引数     :無し
+  //戻り値   :bool       :黒色検知 = true
   bool getBlackColor();  //黒色検知関数
 };
