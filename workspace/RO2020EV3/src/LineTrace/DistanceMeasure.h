b8dad6b8f4b2ed971cf05ac1cd73aa9239bbf574
Nagisa Takeuchi <takeuchi.nagisa1@gmail.com>
2019-07-18 01:36:43 +0900

    update:関数ヘッダコメント追加

--- workspace/RO2020EV3/src/LineTrace/DistanceMeasure.h
+++ workspace/RO2020EV3/src/LineTrace/DistanceMeasure.h
@@ -12,6 +12,11 @@ class DistanceMeasure {
  public:
   DistanceMeasure();              //コンストラクタ
   ~DistanceMeasure();             //デストラクタ
+  //関数名   :getDistance
+  //機能名   :距離計測
+  //機能概要 :MWを呼び出し、累計走行距離を取得する
+  //引数     :無し
+  //戻り値   :float               :累計走行距離
   static float getDistance();     //累計距離取得関数
 
  private:
