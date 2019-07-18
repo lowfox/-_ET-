52d2f34a7b8b548419417c318d28a76abdf5f6f1
Nagisa Takeuchi <takeuchi.nagisa1@gmail.com>
2019-07-18 17:10:15 +0900

    update:DistanceMeasureのテストコードをrunに記載

--- workspace/RO2020EV3/src/LineTrace/LineTraceScene.cpp
+++ workspace/RO2020EV3/src/LineTrace/LineTraceScene.cpp
@@ -1,17 +1,39 @@
 #include "LineTraceScene.h"
 #include <Config.h>
+// ColorDetectionTest.cpp
+// LineTraceSceneにそのまま張り付ければ使用可能
 
-LineTraceScene::LineTraceScene(ISceneChanger* sceneChanger) :IScene(sceneChanger)
-{
-}
+//テストコード*****************************************
 
-bool LineTraceScene::init()
-{
-	return true;
-}
+#include <Logger.h>
+#include "ColorDetection.h"
 
-bool LineTraceScene::run()
-{
+LineTraceScene::LineTraceScene(ISceneChanger* sceneChanger)
+    : IScene(sceneChanger) {}
+
+bool LineTraceScene::init() { return true; }
+
+bool LineTraceScene::run() {
+  // LineTraceCode
+
+  //実機に入れて、センサーを色の上に乗せればログが出るはず。
+  //テストコードStart******************************************
+
+  // ColorDetectionクラス生成
+  ColorDetection colorDetection;
+
+  while (1) {
+    if (colorDetection.getBlackColor() == true) {
+      EV3_LOG("Now Black\n");
+    } else if (colorDetection.getBlueColor() == true) {
+      EV3_LOG("Now Blue\n");
+    } else {
+      EV3_LOG("No Color\n");
+    }
+  }
+
+  EV3_LOG("End\n");
+  //テストコードEnd*************************************************
 
   return change(LINETRACE_NEXT_SCENE);
 }
