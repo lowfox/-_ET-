7bbc3eea32eaf24ce8601a4f01d22d8d595a41b6
Nagisa Takeuchi <takeuchi.nagisa1@gmail.com>
2019-08-21 19:50:38 +0900

    update:LineTraceScene.cppを本番用に変更(動作確認)

--- workspace/RO2020EV3/src/LineTrace/LineTraceScene.cpp
+++ workspace/RO2020EV3/src/LineTrace/LineTraceScene.cpp
@@ -1,12 +1,16 @@
 #include "LineTraceScene.h"
 #include <Config.h>
-
+#include "Standby.h"
 LineTraceScene::LineTraceScene(ISceneChanger* sceneChanger)
-    : IScene(sceneChanger) {}
+  : IScene(sceneChanger) {}
 
 bool LineTraceScene::init() { return true; }
 
 bool LineTraceScene::run() {
   // LineTraceCode
+  Standby stanby;       //Stanbyクラスのインスタンス生成
+
+  stanby.traceMain();       //setup関数開始
+
   return change(LINETRACE_NEXT_SCENE);
 }
