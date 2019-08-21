f97e360633816c5e0437f4c8a488f1bd1ca0745c
Nagisa Takeuchi <takeuchi.nagisa1@gmail.com>
2019-08-21 19:54:28 +0900

    update:StartDash.cpp尻尾操作関連修正

--- workspace/RO2020EV3/src/LineTrace/StartDash.cpp
+++ workspace/RO2020EV3/src/LineTrace/StartDash.cpp
@@ -7,7 +7,7 @@ StartDash::~StartDash() {}
 
 void StartDash::tailOperation() {
 	// しっぽをあげる
-	RyujiEv3Engine::GetTailMotor()->setCounts(0, 50);
+	RyujiEv3Engine::GetTailMotor()->setCounts(-90, 50,true);      //Takeuchi(尻尾を90°上げる)
 }
 
 //int StartDash::gyroOffsetSetting() {}
