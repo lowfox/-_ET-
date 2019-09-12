e5a927a246618b323d8f98dac5196260ef9ab5f9
deodorantpower <yuta.0401.hsgw@gmail.com>
2019-09-13 00:38:02 +0900

    安定化完了

---
+++ docker/home/build.py
@@ -0,0 +1,43 @@
+"""カレントディレクトリから再帰的に検索してcpp/hにclang-formatをする。"""
+
+import glob
+from datetime import datetime, timedelta, timezone
+import os
+import subprocess
+
+BASE_DIRS = ["/hrp2/sdk/common/library", "/hrp2/sdk/workspace"]
+
+# TODO: ミドルウェアのビルドが失敗する原因を突き止めて有効にする
+# print('**** clang-format ****')
+# for bd in BASE_DIRS:
+#     cpp_list = glob.glob(bd+'/**/*.cpp', recursive=True)
+#     header_list = glob.glob(bd+'/**/*.h', recursive=True)
+#     cmd = "clang-format -style=file -i {}"
+
+#     for cpp in cpp_list:
+#         print(cmd.format(cpp))
+#         subprocess.call(cmd.format(cpp), shell=True)
+
+#     for header in header_list:
+#         print(cmd.format(header))
+#         subprocess.call(cmd.format(header), shell=True)
+
+
+print('**** EV3 build ****')
+ENV = "EV3_APP_NAME"
+name = "app"
+if ENV in os.environ:
+    name = os.environ[ENV]
+
+subprocess.call("rm -r /gateway/library/RyujiEv3", shell=True)
+subprocess.call("cp -r /hrp2/sdk/common/library/RyujiEv3 /gateway/library", shell=True)
+subprocess.call("rm -r /gateway/workspace/RO2020EV3", shell=True)
+subprocess.call("cp -r /hrp2/sdk/workspace/RO2020EV3 /gateway/workspace", shell=True)
+
+now = datetime.now(timezone(timedelta(hours=+9), 'JST'))
+name += now.strftime('%m%d%H%M%S')
+os.chdir("/hrp2/sdk/workspace")
+subprocess.call("make app=RO2020EV3", shell=True)
+if not os.path.exists("/gateway/production"):
+    os.mkdir("/gateway/production")
+subprocess.call("mv /hrp2/sdk/workspace/app /gateway/production/" + name, shell=True)
